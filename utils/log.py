64d7f39e42af56902191d62bf99ed28ee56e8245
KnightZhang <knight.625@live.com>
2019-08-02 14:13:06 +0800

    add log

---
+++ utils/log.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+def log_info(message):
+    print('\033[1;34m Info : {} \033[0m'.format(message))
+
+def log_error(message):
+    print('\033[1;31m Error : {} \033[0m'.format(message))
+
