5744d3c8fa615ecbeb637fe272d46ca6b7a29e47
KnightZhang <knight.625@live.com>
2019-08-02 14:11:42 +0800

    write base model

---
+++ model.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+# Produced by Andysin Zhang
+# 02_Aug_2019
+
+'''this is the fundamental model on which our model is based on'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import tensorflow as tf
+
+class BaseModel(object):
+	pass
