534d69b19d20a73a18a821a2ba53a2ea675dbc44
KnightZhang <knight.625@live.com>
2019-08-08 11:27:13 +0800

    Doing : Encoder, TODO : build graph

--- base_model.py
+++ base_model.py
@@ -26,40 +26,174 @@ import abc
 import collections
 import tensorflow as tf
 
+import model_helper as _mh
+
 from utils.log import log_info as _info
 from utils.log import log_error as _error
 
 def get_scpecific_scope_params(scope=''):
-    """used to get specific parameters for training
-    """
-    return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
+	"""used to get specific parameters for training
+	"""
+	return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
 
 class TrainOutputTuple(collections.namedtuple('TrainOutputTuple', 'a b c')):
-    # TODO
-    pass
+	# TODO
+	pass
 
 class EvalOutputTuple(collections.namedtuple('EvalOutputTuple', 'a b')):
-    # TODO
-    pass
+	# TODO
+	pass
 
 class InferOutputTuple(collections.namedtuple('InferOutputTuple', 'a')):
-    # TODO
-    pass
+	# TODO
+	pass
 
 class BaseModel(object):
-    """Base Abstract Model, 
-    """
-    def __init__(self, hparams, mode, scope=None):
-        
-        # TODO 1. parse the hparams
-        # TODO 2. build the graph
-        # TODO 3. Saver
-        pass
-        
-    def _set_params_initializer(self):
-    # TODO
-        pass
+	"""Base Model 
+	"""
+	def __init__(self, hparams, mode, scope=None):
+		
+		self._set_params_initializer(hparams, mode, scope)
+		# TODO 2. build the graph
+		# TODO 3. Saver
+		pass
+
+	def _set_params_initializer(self, hparams, mode, scope):
+		"""Load the parameters and set the initializer
+		"""
+		self.mode = mode
+		# pre_train flag is used for distinguish with pre_train and fine tune
+		self.pre_train = hparams.pre_train
+		self.dtype = tf.float32
+		self.global_step = tf.Variable(0, trainable=False)
+
+		# define the input for the model
+		self.encoder_input_data = tf.placeholder(
+			tf.int32, [None, None], name='encoder_input_data')
+		self.decoder_input_data = tf.placeholder(
+			tf.int32, [None, None], name='decoder_input_data')
+		self.decoder_output_data = tf.placeholder(
+			tf.int32, [None, None], name='decoder_output_data')
+		self.seq_length_encoder_intput_data = tf.placeholder(
+			tf.int32, [None], name='seq_length_encoder_input_data')
+		self.seq_length_decoder_input_data = tf.placeholder(
+			tf.int32, [None], name='seq_length_decoder_input_data')
+		
+		# load some important hparamters
+		self.num_units = hparams.num_units
+		self.num_encoder_layers = hparams.num_encoder_layers
+		self.num_decoder_layers = hparams.num_decoder_layers
+		self.num_encoder_residual_layers = self.num_encoder_layers - 1
+		self.num_decoder_residual_layers = self.num_decoder_layers - 1
+
+		self.batch_size = hparams.batch_size
+
+		# set initializer
+		random_seed = hparams.random_seed
+		initializer = _mh.get_initializer(hparams.init_op, random_seed, hparams.init_weight)
+		tf.get_variable_scope().set_initializer(initializer)
+
+		# embeddings
+		self.src_vocab_size = hparams.src_vocab_size
+		self.tgt_vocab_size = hparams.tgt_vocab_size
+		self.init_embeddings(hparams, scope)
+
+	def init_embeddings(self, hparams, scope):
+		"""Init embeddings
+		"""
+		self.embedding_encoder, self.embedding_decoder = \
+			_mh.create_emb_for_encoder_and_decoder(
+				share_vocab=hparams.share_vocab,
+				src_vocab_size=self.src_vocab_size,
+				tgt_vocab_size=self.tgt_vocab_size,
+				src_embed_size=self.num_units,
+				tgt_embed_size=self.num_units,
+				scope=scope)
+
+	def build_graph(self, hparams, scope):
+		_info('Start build {} graph ...'.format(self.mode))
+
+		with tf.variable_scope('dynamic_seq2seq', dtype=self.dtype):
+			# Encoder
+			encoder_outputs, encoder_state = self._build_encoder(hparams)
+	
+	def _train_or_inference(self):
+		# TODO 
+		# need to optimize, etc. in train
+		# used for seperate process in train and test
+		pass
+	
+	def _build_encoder(self, hparams):
+		num_layers = self.num_encoder_layers
+		num_redisual_layers = self.num_encoder_residual_layers
+
+		with tf.variable_scope('encoder') as scope:
+			self.encoder_emb_inp = tf.embedding_loopup(self.embedding_encoder, self.encoder_input_data)
+
+			if hparams.encoder_type == 'uni':
+				_info('num_layers = {} num_residual_layers = {}'.format(num_layers, num_redisual_layers))
+				cell = self._build_encoder_cell(hparams, num_layers, num_redisual_layers)
+
+	def _build_encoder_cell(self, hparams, num_layers, num_residual_layers):
+		"""Build a multi-layer RNN cell
+		"""
+		return _mh.create_rnn_cell(
+			unit_type=hparams.unit_type,
+			num_units=self.num_units,
+			num_layers=num_layers,
+			num_residual_layers=num_residual_layers
+			forget_bias=hparams.forget_bias,
+			dropout=hparams.dropout,
+			mode=self.mode)
+
+	def _get_infer_maximum_iterations(self):
+		# TODO
+		pass
+	
+	def _build_decoder(self):
+		# TODO
+		pass
+	
+	def _build_decoder_cell(self):
+		# TODO
+		pass
+
+	def _cross_entropy_loss(self):
+		# TODO
+		pass
+
+	def _vae_loss(self):
+		# TODO
+		pass
+	
+	def _compute_loss(self):
+		# TODO
+		pass
+		
+	def _get_learning_rate_warmup(self):
+		# TODO
+		pass
+	
+	def _get_learning_rate_decay(self):
+		# TODO
+		pass
+	
+	def _get_train_summary(self):
+		# TODO
+		pass
+		
+	def train(self):
+		# TODO
+		pass
+	
+	def eval(self):
+		# TODO
+		pass
+	
+	def infer(self):
+		# TODO
+		pass
 
 if __name__ == '__main__':
-    t = TrainOutputTuple(a='a', b='bb', c='ccc')
-    print(t)
+	t = TrainOutputTuple(a='a', b='bb', c='ccc')
+	print(t)
--- model_helper.py
+++ model_helper.py
@@ -15,46 +15,120 @@ from utils.log import log_error as _error
 __all__ = ['get_initializer', 
            'create_emb_for_encoder_and_decoder']
 
-def get_initializer(init_name, seed=None, init_weight=None):
-    """create the initializer
+def get_initializer(self, init_op, random_seed=None, init_weight=None):
+    """Set the initializer
+
+    Args:
+        init_op: 'orthogonal' | 'normal' | 'uniform'
+    
+    Returns:
+        Initializer
+    
+    Raises:
+        ValueError: Unknown initializer flag
     """
-    if init_name == 'uniform':
-        assert init_weight
-        return tf.random_uniform_initializer(
-            -init_weight, init_weight, seed=seed)
-    elif init_name == 'glorot_normal':
-        return tf.keras.initializer.glorot_normal(
-            seed=seed)
-    elif init_name == 'glorot_uniform':
-        return tf.keras.initiazer.glorot_uniform(
-            seed=seed)
+    if init_op == 'orthogonal':
+        return tf.initializers.orthogonal(seed=random_seed)
+    elif init_op == 'normal':
+        return tf.random_normal_initializer(seed=random_seed)
+    elif init_op == 'uniform':
+        return tf.random_uniform_initializer(-init_weight, init_weight, seed=random_seed)
     else:
-        _info('Unknow {} initializer'.format(init_name))
+        _error('Unknown initializer {}'.format(init_op))
         raise ValueError
-        
-def create_emb_for_encoder_and_decoder(src_vocab_size, tgt_vocab_size, embed_size, share_vocab):
-    """create embedding matrinx
+
+def create_emb_for_encoder_and_decoder(share_vocab,
+                                       src_vocab_size,
+                                       tgt_vocab_size,
+                                       src_embed_size,
+                                       tgt_embed_size,
+                                       scope):
+    """Create the embedding
+
+    Args:
+        share_vocab: whether source and target use the same embedding matrix
+        src_vocab_size: source vocab size
+        tgt_vocab_size: target vocab size
+        src_embed_size: source embedding dims
+        tgt_embed_size: target embedding dims
+        scope: VariableScope
+    
+    Returns:
+        embedding_encoder & embedding_decoder
+    
+    Raises:
+        ValueError: if share_vocab == True, src_vocab_size != tgt_vocab_size
     """
-    with tf.variable_scope('embeddings') as _:
+    with tf.variable_scope('embedding', dtype=tf.float32) as scope:
         if share_vocab:
-            if src_vocab_size != tgt_vocab_size:
-                _error('src_vocab_size must be identical to tgt_vocab_size when share_vocab is True')
+            if not src_vocab_size == tgt_vocab_size:
+                _error('Source vocab size must be equal to Target vocab size, however, {} to {}'
+                    .format(src_vocab_size, tgt_vocab_size))
                 raise ValueError
-            _info('Using the same embedding for source and target')
-            embedding = tf.get_variable('embedding', [src_vocab_size, embed_size], dtype=tf.float32)
-            return embedding, embedding
+                _info('Use the same embeddings for source and target')
+                embedding_encoder = tf.get_variable('embedding_shared', [src_vocab_size, src_embed_size], dtype=tf.float32)
+                embedding_decoder = embedding_encoder
         else:
-            _info('Using the different embeddings for source and target respectively')
-            with tf.variable_scope('encoder') as _:
-                embedding_encoder = tf.get_variable(
-                    'embedding_encoder', [src_vocab_size, embed_size], dtype=tf.float32)
-            with tf.variable_scope('decoder') as _:
-                embedding_decoder = tf.get_variable(
-                    'embedding_decoder', [tgt_vocab_size, embed_size], dtype=tf.float32)
-            return embedding_encoder, embedding_decoder
+            with tf.variable_scope('encoder'):
+                embedding_encoder = tf.get_variable('embedding_encoder', [src_vocab_size, src_embed_size], dtype=tf.float32)
+            with tf.variable_scope('decoder'):
+                embedding_decoder = tf.get_varialbe('embedding_decoder', [tgt_vocab_size, tgt_embed_size], dtype=tf.float32)
+    return embedding_encoder, embedding_decoder
 
-def create_rnn_cell(unit_type, num_units, num_layers,)
+def create_rnn_cell(unit_type,
+                    num_units,
+                    num_layers,
+                    num_residual_layers,
+                    forget_bias,
+                    dropout,
+                    mode):
+    """Create multi-layer RNN cell
+    """
+    cell_list = _create_cell_list(unit_type=unit_type,
+                                  num_units=num_units,
+                                  num_layers=num_layers,
+                                  num_residual_layers=num_residual_layers,
+                                  forget_bias=forget_bias,
+                                  dropout=dropout,
+                                  mode=mode)
+    
+def _create_cell_list(unit_type,
+                      num_units,
+                      num_layers,
+                      num_residual_layers,
+                      forget_bias,
+                      dropout,
+                      mode):
+    cell_list = []
+    for i in range(num_layers):
+        _info('Building {}th cell'.format(i))
+        cell = _create_single_cell(
+            unit_type=unit_type,
+            num_units=num_units,
+            forget_bias=forget_bias,
+            dropout=dropout,
+            mode=mode,
+            resudual_or_not=(i >= num_layers - num_residual_layers))
 
+def _create_single_cell(unit_type,
+                        num_units,
+                        forget_bias,
+                        dropout,
+                        mode,
+                        residual_or_not):
+    """Create a cell
+    """
+    dropout = dropout if mode == 'train' else 0.and
+
+    if unit_type == 'lstm':
+        _info(' build lstm cell')
+        cell = tf.nn.rnn_cell.BasicLSTMCell(num_units, forget_bias=forget_bias)
+    elif unit_type == 'gru':
+        _info(' build gru cell')
+        cell = tf.nn.rnn_cell.GRUCell(num_units)
+    else:
+        _error('Unknow unit type : {}'.format(unit_type))
+    
 
 if __name__ == "__main__":
     get_initializer('dad')
