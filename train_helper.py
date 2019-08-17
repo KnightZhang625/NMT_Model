b6dc57adaf18478222ce7db88cd8f8372f33e3e0
KnightZhang <knight.625@live.com>
2019-08-17 18:15:43 +0800

    DONE: feed data to fine tune step TODO: convert str to idx in fine tune step

--- train_helper.py
+++ train_helper.py
@@ -34,23 +34,42 @@ class DataTuple(collections.namedtuple('data',
                                  seq_length_decoder_data')):
     pass
 
+class QueAnsTuple(collections.namedtuple('que_ans',
+                                    'question \
+                                     answer')):
+    pass
+
 def _find_files(path):
     """return the files in the given directory and the directory"""
     dir_path = path
     return dir_path, sorted(os.listdir(path), key=find_num)
 
-def  _convert_str_to_int(string):
+def _convert_str_to_int_fine_tune(data_tuple):
+    """different solution to the fine tune step"""
+    question_int = _convert_str_to_int(data_tuple.question)
+    answer_int = _convert_str_to_int(data_tuple.answer)
+    return QueAnsTuple(question=question_int, answer=answer_int)
+
+def  _convert_str_to_int(string, pre_train=True):
     """convert string to int"""
-    return [int(s) for s in string.split(' ')]
-    
+    if pre_train:
+        return [int(s) for s in string.split(' ')]
+    else:
+        return _convert_str_to_int_fine_tune(string)
+
 insert_sos = lambda l : [SOS] + l
 insert_eos = lambda l : l + [EOS]
 
-def _create_data(data):
+def _create_data(data, pre_train=True):
     """used for creating inputs, outputs, seq_len"""
-    encoder_input_data = data
-    decoder_input_data = list(map(insert_sos, data))
-    decoder_output_data = list(map(insert_eos, data))
+    if pre_train:
+        encoder_input_data = data
+        decoder_input_data = list(map(insert_sos, data))
+        decoder_output_data = list(map(insert_eos, data))
+    else:
+        encoder_input_data = [d.question for d in data]
+        decoder_input_data = list(map(insert_sos, [d.answer for d in data]))
+        decoder_output_data = list(map(insert_eos, [d.answer for d in data]))
     seq_length_encoder_data = [len(d) for d in encoder_input_data]
     seq_length_decoder_data = [len(d) for d in decoder_input_data]
 
@@ -68,46 +87,84 @@ def _create_data(data):
                            seq_length_decoder_data=seq_length_decoder_data)
     return data_tuple
 
-def iter_data(path):
-    """load the data with batch size for training"""
-    data_bz = []
-    dir_path, files_list = _find_files(path)
-    
-    data_temp = []
-    for file_name in files_list:
-        _info('Processing {}'.format(file_name))
-        with codecs.open(str(dir_path / file_name), 'r', 'utf-8') as file:
-            data = file.read().split('\n')[:-1]
-        
-        # check whether the length of data could be divided by the batch size or not
-        if len(data) % BATCH_SIZE != 0:
-            divide_or_not = False
-        else:
-            divide_or_not = True
+_split_que_ans = lambda line : QueAnsTuple(question=line.split('=')[0],
+                                           answer=line.split('=')[1])
 
-        batch_number = len(data) // BATCH_SIZE
+def iter_data(path, pre_train=True):
+    """load the data with batch size for training"""
+    if pre_train:
+        data_bz = []
+        dir_path, files_list = _find_files(path)
+        
+        data_temp = []
+        for file_name in files_list:
+            _info('Processing {}'.format(file_name))
+            with codecs.open(str(dir_path / file_name), 'r', 'utf-8') as file:
+                data = file.read().split('\n')[:-1]
+            
+            # check whether the length of data could be divided by the batch size or not
+            if len(data) % BATCH_SIZE != 0:
+                divide_or_not = False
+            else:
+                divide_or_not = True
+
+            batch_number = len(data) // BATCH_SIZE
+            for bn in range(batch_number):
+                data_block = data[bn * BATCH_SIZE : bn * BATCH_SIZE + BATCH_SIZE]
+                data_block = list(map(_convert_str_to_int, data_block))
+                yield _create_data(data_block)
+            # if not, random sample data from the rest to let the length equal to the batch size
+            if not divide_or_not:
+                bn = batch_number
+                data_block = data[bn * BATCH_SIZE : ]
+                try:
+                    data_sup = random.sample(data_block, BATCH_SIZE - len(data_block))
+                except ValueError:
+                    # except the circumstance where the sample number is larger then the original length
+                    data_sup = [random.choice(data_block) for _ in range(BATCH_SIZE - len(data_block))]
+                data_block += data_sup
+                data_block = list(map(_convert_str_to_int, data_block))
+                yield _create_data(data_block)
+    else:
+        # open the file, split the files to question and answer
+        with codecs.open(path, 'r', 'utf-8') as file:
+            lines = file.read().split('\n')
+            lines = list(map(_split_que_ans, lines))
+            _info('Successful open {}'.format(path), 'OPEN FILE')
+        # create batches and return them      
+        batch_number = len(lines) // BATCH_SIZE
+        if len(lines) % BATCH_SIZE == 0:
+            divide_or_not = True
+        else:
+            divide_or_not = False
+        
         for bn in range(batch_number):
-            data_block = data[bn * BATCH_SIZE : bn * BATCH_SIZE + BATCH_SIZE]
-            data_block = list(map(_convert_str_to_int, data_block))
-            yield _create_data(data_block)
-        # if not, random sample data from the rest to let the length equal to the batch size
+            lines_block = lines[bn * BATCH_SIZE : bn * BATCH_SIZE + BATCH_SIZE]
+            for idx, line in enumerate(lines_block):
+                lines_block[idx] = _convert_str_to_int(line, pre_train)
+            yield _create_data(lines_block, pre_train)
+
         if not divide_or_not:
             bn = batch_number
-            data_block = data[bn * BATCH_SIZE : ]
+            lines_block = lines[bn * BATCH_SIZE : ]
             try:
-                data_sup = random.sample(data_block, BATCH_SIZE - len(data_block))
+                lines_sup = random.sample(lines_block, BATCH_SIZE - len(lines_block))
             except ValueError:
                 # except the circumstance where the sample number is larger then the original length
-                data_sup = [random.choice(data_block) for _ in range(BATCH_SIZE - len(data_block))]
-            data_block += data_sup
-            data_block = list(map(_convert_str_to_int, data_block))
-            yield _create_data(data_block)
-    
+                lines_sup = [random.choice(lines_block) for _ in range(BATCH_SIZE - len(lines_block))]
+            lines_block += lines_sup
+            for idx, line in enumerate(lines_block):
+                lines_block[idx] = _convert_str_to_int(line, pre_train)
+            yield _create_data(lines_block, pre_train)
+
 if __name__ == '__main__':
-    data_path = cur_path / 'data/news_data'
+    # data_path = cur_path / 'data/news_data'
 
     # i_s = lambda s : str(s)
     # test = lambda l : ' '.join(list(map(i_s, l)))
 
-    for data in iter_data(data_path):
-        print('success')
+    # for data in iter_data(data_path):
+    #     print('success')
+
+    for data in iter_data('test.txt', pre_train=False):
+        print(data)
