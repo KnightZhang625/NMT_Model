8f41c084ae391a9777ec6105b4d294c02071ee79
KnightZhang <knight.625@live.com>
2019-08-17 14:49:04 +0800

    could define head message in log api

--- utils/log.py
+++ utils/log.py
@@ -2,9 +2,12 @@
 
 from __future__ import print_function
 
-def log_info(message):
-    print('\033[1;34m Info : {} \033[0m'.format(message))
+def log_info(message, head='INFO'):
+    print('\033[1;34m {} : {} \033[0m'.format(head, message))
 
-def log_error(message):
-    print('\033[1;31m Error : {} \033[0m'.format(message))
+def log_error(message, head='INFO'):
+    print('\033[1;31m {} : {} \033[0m'.format(head, message))
 
+if __name__ == '__main__':
+    log_info('This is a test')
+    log_info('user-defined test', head='USER')
